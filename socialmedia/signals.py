db1092c6b95de42af40c76461b3cf3a07d4a005a
Saalim Aqueel <aqueel192@gmail.com>
2026-06-10 16:59:04 +0530

    register dashboard component dynamically using event_dashboard_components signal

--- socialmedia/signals.py
+++ socialmedia/signals.py
@@ -1,5 +1,10 @@
 from django.dispatch import receiver
-from eventyay.control.signals import nav_event, nav_global
+from django.template.loader import render_to_string
+from eventyay.control.signals import (
+    event_dashboard_components,
+    nav_event,
+    nav_global,
+)
 
 
 @receiver(nav_global, dispatch_uid="socialmedia_nav")
@@ -10,3 +15,12 @@ def control_nav_socialmedia(sender, request=None, **kwargs):
 @receiver(nav_event, dispatch_uid="socialmedia_nav_event")
 def control_nav_event_socialmedia(sender, request=None, **kwargs):
     return []
+
+
+@receiver(event_dashboard_components, dispatch_uid="socialmedia_dashboard_components")
+def control_dashboard_socialmedia(sender, request=None, **kwargs):
+    return render_to_string(
+        "socialmedia/dashboard_component.html",
+        {"request": request},
+        request=request,
+    )
