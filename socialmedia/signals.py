6dd1b57ec9f6484b613911de6a52f5de1b40ad06
Saalim Aqueel <aqueel192@gmail.com>
2026-06-10 22:36:21 +0530

    feat: add social media CSV generator with live post preview UI

--- socialmedia/signals.py
+++ socialmedia/signals.py
@@ -1,19 +1,11 @@
 from django.dispatch import receiver
 from django.template.loader import render_to_string
-from eventyay.control.signals import (
-    event_dashboard_components,
-    nav_event,
-    nav_global,
-)
+
+from eventyay.control.signals import event_dashboard_components, nav_global
 
 
 @receiver(nav_global, dispatch_uid="socialmedia_nav")
 def control_nav_socialmedia(sender, request=None, **kwargs):
-    return []
-
-
-@receiver(nav_event, dispatch_uid="socialmedia_nav_event")
-def control_nav_event_socialmedia(sender, request=None, **kwargs):
     return []
 
 
