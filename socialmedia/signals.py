99a7156cfe36b70e146f0be570b1cc3bec77ec4c
Saalim Aqueel <aqueel192@gmail.com>
2026-06-13 21:39:37 +0530

    feat: use nav_event_common directly for dashboard sidebar navigation

--- socialmedia/signals.py
+++ socialmedia/signals.py
@@ -4,14 +4,9 @@ from django.urls import resolve, reverse
 from django.utils.translation import gettext_lazy as _
 from eventyay.control.signals import (
     event_dashboard_components,
-    nav_event,
+    nav_event_common,
     nav_global,
 )
-
-try:
-    from eventyay.control.signals import nav_event_common
-except ImportError:
-    nav_event_common = None
 
 
 @receiver(nav_global, dispatch_uid="socialmedia_nav")
@@ -19,34 +14,27 @@ def control_nav_socialmedia(sender, request=None, **kwargs):
     return []
 
 
-@receiver(nav_event, dispatch_uid="socialmedia_nav_event")
-def control_nav_event_socialmedia(sender, request=None, **kwargs):
-    return []
-
-
-if nav_event_common:
-
-    @receiver(nav_event_common, dispatch_uid="socialmedia_nav_event_common")
-    def control_nav_event_common_socialmedia(sender, request=None, **kwargs):
-        if not request or not request.user.is_authenticated:
-            return []
-        url = resolve(request.path_info)
-        return [
-            {
-                "label": _("Social Media"),
-                "url": reverse(
-                    "plugins:socialmedia:index",
-                    kwargs={
-                        "organizer": sender.organizer.slug,
-                        "event": sender.slug,
-                    },
-                ),
-                "icon": "share-alt",
-                "active": (
-                    url.namespace == "plugins:socialmedia" and url.url_name == "index"
-                ),
-            }
-        ]
+@receiver(nav_event_common, dispatch_uid="socialmedia_nav_event_common")
+def control_nav_event_common_socialmedia(sender, request=None, **kwargs):
+    if not request or not request.user.is_authenticated:
+        return []
+    url = resolve(request.path_info)
+    return [
+        {
+            "label": _("Social Media"),
+            "url": reverse(
+                "plugins:socialmedia:index",
+                kwargs={
+                    "organizer": sender.organizer.slug,
+                    "event": sender.slug,
+                },
+            ),
+            "icon": "share-alt",
+            "active": (
+                url.namespace == "plugins:socialmedia" and url.url_name == "index"
+            ),
+        }
+    ]
 
 
 @receiver(event_dashboard_components, dispatch_uid="socialmedia_dashboard_components")
